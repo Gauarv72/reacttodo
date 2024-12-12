af6c71ac242cd2f21cf88fd4e1fde3b2c2ded1e8
gaurav kumar <gauravkumar28312@gmail.com>
2024-12-12 14:36:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import AddTodo from './components/addTodo'
+import Todos from './components/todos'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Learn About Redux toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
